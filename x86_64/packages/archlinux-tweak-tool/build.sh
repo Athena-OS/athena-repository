c18bc73822504e144c39d737739a68406055bb0f
D3vil0per <>
2022-06-22 00:49:59 +0200

    Uploaded signed packages

--- x86_64/packages/archlinux-tweak-tool/build.sh
+++ x86_64/packages/archlinux-tweak-tool/build.sh
@@ -1,27 +1,18 @@
 #!/bin/sh
 
-#NEED ONLY TO EDIT  sourcefiles VARIABLE
+git clone https://aur.archlinux.org/archlinux-tweak-tool-git.git
 
-sourcefiles="usr"
+cd archlinux-tweak-tool-git
 
-pkgname=$(grep "^pkgname=" PKGBUILD | awk -F'"' '{print $2}')
-pkgver=$(grep "^pkgver=" PKGBUILD | awk -F'"' '{print $2}')
-pkgrel=$(grep "^pkgrel=" PKGBUILD | awk -F'"' '{print $2}')
-arch=$(grep "^arch=" PKGBUILD | awk -F'"' '{print $2}')
-
+pkgname=$(grep "^pkgname=" PKGBUILD | cut -d "=" -f2)
+pkgver=$(grep "^pkgver=" PKGBUILD | cut -d "=" -f2)
+pkgrel=$(grep "^pkgrel=" PKGBUILD | cut -d "=" -f2)
+arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
 pkgfile=$pkgname-$pkgver-$pkgrel-$arch.pkg.tar.zst
 
-echo $pkgfile
+makepkg -sr --sign
 
-sed -i '/^sha256/d' PKGBUILD
+mv *.zst *.zst.sig ../../../
 
-tar -zcvf $pkgname.tar.gz $sourcefiles 
-
-makepkg -g >> PKGBUILD
-makepkg -f -sr --sign
-
-rm -rf src pkg $pkgname.tar.gz
-
-rm -rf ../../$pkgfile
-
-mv $pkgfile $pkgfile.sig ../../
+cd ..
+rm -rf archlinux-tweak-tool-git
