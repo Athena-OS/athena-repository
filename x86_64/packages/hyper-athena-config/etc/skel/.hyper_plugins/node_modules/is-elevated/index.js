d798283e41caf4c5793b9169ec204a53dd8a01fb
D3vil0per <>
2022-07-11 01:52:24 +0200

    Deleted AUR packages because we'll use Chaotic repo by pacman

--- x86_64/packages/hyper-athena-config/etc/skel/.hyper_plugins/node_modules/is-elevated/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const isRoot = require('is-root');
-const isAdmin = require('is-admin');
-
-module.exports = () => process.platform === 'win32' ? isAdmin() : Promise.resolve().then(() => isRoot());
