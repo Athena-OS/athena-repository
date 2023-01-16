9db9ee81866980de60031ebecc7c2d08ede07a61
D3vil0p3r <>
2023-01-16 15:49:35 +0100

    Added shello tool

--- x86_64/packages/athena-roles/athena-blackhat/gen_tools.sh
+++ x86_64/packages/athena-roles/athena-blackhat/gen_tools.sh
@@ -8,5 +8,5 @@ sed -i '/^$/d' PKGBUILD #Delete all empty lines
 printf '\n' >> PKGBUILD #Avoid that the new "depends" string is placed on an existing row
 printf 'depends=( ' >> PKGBUILD
 printf "'%s' " $(pacman -Sgq blackarch | awk '!seen[$0]++' | sed -e "$(sed 's:.*:s/^&$//ig:' <<<$(printf "%s\n" "${to_exclude[@]}"))") >> PKGBUILD
-printf "'ians' 'ratinject' " >> PKGBUILD
+printf "'ians' 'ratinject' 'shello' " >> PKGBUILD
 printf ')\n' >> PKGBUILD
